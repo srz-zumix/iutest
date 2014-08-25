95ecdacfc4dba14778479474a656203d3dc5b1a5
srz-zumix <zumix.cpp@gmail.com>
2014-08-25 19:33:25 +0900

    update r688

--- include/iutest_ver.hpp
+++ include/iutest_ver.hpp
@@ -17,11 +17,11 @@
 
 //======================================================================
 // define
-#define	IUTEST_VER			0x01099927u	//!< iutest version 1.9.99.27
+#define	IUTEST_VER			0x01100000u	//!< iutest version 1.10.0.0
 #define IUTEST_MAJORVER		0x01u		//!< Major Version
-#define IUTEST_MINORVER		0x09u		//!< Minor Version
-#define IUTEST_BUILD		0x99u		//!< Build
-#define IUTEST_REVISION		0x27u		//!< Revision
+#define IUTEST_MINORVER		0x10u		//!< Minor Version
+#define IUTEST_BUILD		0x00u		//!< Build
+#define IUTEST_REVISION		0x00u		//!< Revision
 
 /**
  * @mainpage
