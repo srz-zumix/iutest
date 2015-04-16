f8af701ec134608b351bce99095353ea2c846a26
srz-zumix <zumix.cpp@gmail.com>
2015-04-17 07:53:07 +0900

    update r818

---
+++ tools/twilio/sample.cpp
@@ -0,0 +1,33 @@
+﻿//======================================================================
+//-----------------------------------------------------------------------
+/**
+ * @file		sample.cpp
+ * @brief		twilio sample
+ *
+ * @author		t.shirayanagi
+ * @par			copyright
+ * Copyright (C) 2015, Takazumi Shirayanagi\n
+ * This software is released under the new BSD License,
+ * see LICENSE
+*/
+//-----------------------------------------------------------------------
+//======================================================================
+#include "../../include/iutest.hpp"
+
+#ifdef UNICODE
+int wmain(int argc, wchar_t* argv[])
+#else
+int main(int argc, char* argv[])
+#endif
+{
+	IUTEST_INIT(&argc, argv);
+	return IUTEST_RUN_ALL_TESTS();
+}
+
+int a=0;
+
+IUTEST(Test, A)
+{
+	IUTEST_ASSERT_EQ(a, 1);
+}
+
