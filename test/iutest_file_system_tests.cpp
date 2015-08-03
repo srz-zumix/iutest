2c1716e2f983a60e1f929873829dd65667f249f0
srz-zumix <zumix.cpp@gmail.com>
2015-08-04 08:35:47 +0900

    update r902

--- test/iutest_file_system_tests.cpp
+++ test/iutest_file_system_tests.cpp
@@ -6,7 +6,7 @@
  *
  * @author		t.shirayanagi
  * @par			copyright
- * Copyright (C) 2014, Takazumi Shirayanagi\n
+ * Copyright (C) 2014-2015, Takazumi Shirayanagi\n
  * This software is released under the new BSD License,
  * see LICENSE
 */
@@ -42,7 +42,11 @@ private:
 	}
 };
 
-static TestFileSystem	s_file_system;
+int RegisterFileSystem()
+{
+	static TestFileSystem s_file_system;
+	return 0;
+}
 
 #endif
 
@@ -53,14 +57,23 @@ int wmain(int argc, wchar_t* argv[])
 int main(int argc, char* argv[])
 #endif
 {
-	IUTEST_INIT(&argc, argv);
 #if !defined(IUTEST_USE_GTEST)
+	{
+		IUTEST_EXPECT_NULL(::iutest::detail::IFileSystem::New());
+		::std::string str;
+		IUTEST_EXPECT_FALSE(::iutest::detail::IFileSystem::ReadAll("iutest_file_system_test.cpp", str));
+	}
+	
+	IUTEST_INIT(&argc, argv);
+
+	RegisterFileSystem();
 	::iutest::IUTEST_FLAG(output) = "xml:test.xml";
 	const int ret = IUTEST_RUN_ALL_TESTS();
 	if( ret != 0 ) return ret;
 	FileSystemCallTest();
 	return ::iutest::UnitTest::GetInstance()->Failed() ? 1 : 0;
 #else
+	IUTEST_INIT(&argc, argv);
 	return IUTEST_RUN_ALL_TESTS();
 #endif
 }
