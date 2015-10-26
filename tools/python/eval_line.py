22ebe5dda46a4581b330f6f0d90a266cb89cbeb2
srz-zumix <zumix.cpp@gmail.com>
2015-10-26 12:18:12 +0900

    update r950

--- tools/python/eval_line.py
+++ tools/python/eval_line.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python
 #
 # eval_line.py
 #
-# Copyright (C) 2014, Takazumi Shirayanagi
+# Copyright (C) 2014-2015, Takazumi Shirayanagi
 # This software is released under the new BSD License,
 # see LICENSE
 #
@@ -9,6 +10,7 @@
 import sys
 import argparse
 import re
+import codecs
 
 from argparse import ArgumentParser
 
@@ -31,7 +33,6 @@ def parse_command_line():
 		'-r'
 		, '--repeat'
 		, action=EvalIntAction
-		, dest = 'repeat'
 		, nargs=1
 		, help = 'Set eval repeat count.'
 		, default = 1
@@ -40,7 +41,6 @@ def parse_command_line():
 		'-s'
 		, '--start'
 		, action=EvalIntAction
-		, dest = 'start'
 		, nargs=1
 		, help = 'Set eval repeat start no.'
 		, default = 0
@@ -48,8 +48,12 @@ def parse_command_line():
 	parser.add_argument(
 		'-o'
 		, '--output'
-		, dest = 'output'
 		, help = 'output file path.'
+		, default = None
+	)
+	parser.add_argument(
+		'--encoding'
+		, help = 'output file encoding.'
 		, default = None
 	)
 	parser.add_argument(
@@ -58,10 +62,7 @@ def parse_command_line():
 		, help = 'eval expressions'
 		, nargs='+'
 	)
-	if len(sys.argv) <= 1:
-		parser.error('invalid number arguments')
-
-	options = parser.parse_args(sys.argv)
+	options = parser.parse_args()
 	return options
 
 def eval_line(r, define, line):
@@ -77,8 +78,8 @@ def eval_line(r, define, line):
 def main():
 	re_eval = re.compile(r'\${([^}]+)}')
 	options = parse_command_line()
-	define = options.expression[1]
-	for a in options.expression[2:]:
+	define = options.expression[0]
+	for a in options.expression[1:]:
 		define += ' ' + a
 
 	output = options.output
@@ -86,7 +87,10 @@ def main():
 		for line in range(options.repeat):
 			print eval_line(re_eval, define, line+options.start)
 	else:
-		file = open(output, 'w')
+		if options.encoding:
+			file = codecs.open(output, 'w', options.encoding)
+		else:
+			file = open(output, 'w')
 		for line in range(options.repeat):
 			file.write( eval_line(re_eval, define, line+options.start) )
 			file.write( '\n' )
