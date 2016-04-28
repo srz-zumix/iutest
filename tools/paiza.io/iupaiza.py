d3583f1b75572d06e8cf03b128942929dcf6779c
srz-zumix <zumix.cpp@gmail.com>
2016-04-29 08:50:37 +0900

    update r1119 update paiza tool

--- tools/paiza.io/iupaiza.py
+++ tools/paiza.io/iupaiza.py
@@ -133,6 +133,179 @@ def output_code(path, code, encoding):
 	f.close()
 
 #
+# 
+def append_define(line, depth, macros, unkowns):
+	def append(d, v, depth, macros, unkowns):
+		d = re.sub('\(.*\)', '', d)
+		if any(x == -1 for x in depth):
+			unkowns.append(d)
+		else:
+			macros[d] = v
+	m = re.match("#\s*define (\S+)\s(.+)$", line)
+	if m:
+		append(m.group(1), m.group(2), depth, macros, unkowns)
+	else:
+		m = re.match("#\s*define (\S+)$", line)
+		if m:
+			append(m.group(1), None, depth, macros, unkowns)
+
+clang_features = { 'cxx_nullptr':'1'
+				 , 'cxx_auto_type':'1' 
+				 , 'cxx_constexpr':'1'
+				 , 'cxx_decltype':'1'
+				 , 'cxx_deleted_functions':'1'
+				 , 'cxx_override_control':'1'
+				 , 'cxx_rvalue_references':'1'
+				 , 'cxx_static_assert':'1'
+				 , 'cxx_variadic_templates':'1'
+				 }
+
+#
+#
+def expand_macro(expr, macros, unkowns):
+	expand = ""
+	for s in re.split('(&&|\|\||!)', expr):
+		if s == '&&':
+			expand += ' and '
+		elif s == '||':
+			expand += ' or '
+		elif s == '!':
+			expand += " not "
+		else:
+			m = re.match("(.*)defined\((.*?)\)(.*)", s)
+			if m:
+				d = m.group(2)
+				if d in unkowns:
+					expand += s
+				else:
+					f = d in macros
+					expand += m.group(1) + str(f) + m.group(3)
+				continue
+			if re.match("(.*)__has_include\((.*?)\)(.*)", s):
+				expand += s
+				continue
+			m = re.match("(.*)__has_feature\((.*?)\)(.*)", s)
+			if m:
+				f = m.group(2)
+				if f in clang_features:
+					expand += m.group(1) + clang_features[f] + m.group(3)
+					continue
+			for w in re.split('([+\-=<>\(\)]+)', s):
+				if re.match('[+\-=<>\(\)]+', w) or w.isspace():
+					expand += w
+				elif len(w) > 0:
+					if w in unkowns:
+						expand += s
+					elif w in macros:
+						expand += expand_macro(macros[w], macros, unkowns)
+					elif w.isdigit():
+						expand += w
+					else:
+						expand += '0'
+
+	expand = expand.replace('0(0)', '0')
+	expand = expand.replace('not =', '!=')
+	return expand
+
+#
+#
+def eval_ppif(expr, macros, unkowns):
+	expand = expand_macro(expr, macros, unkowns)
+	try:
+		if eval(expand):
+			return 1
+		else:
+			return 0
+	except Exception, e:
+		if not any( x in expand for x in unkowns ):
+			print(expr)
+			print(expand)
+			print(e)
+		return -1
+						
+#
+#
+def check_ppif(ins, expr, macros, unkowns):
+	if ins == "if" or ins == "elif":
+		return eval_ppif(expr, macros, unkowns)
+	elif ins == "ifdef":
+		if expr in unkowns:
+			return -1
+		if expr not in macros:
+			return 0
+	elif ins == "ifndef":
+		if expr in unkowns:
+			return -1
+		if expr in macros:
+			return 0
+	return 1
+
+#
+#
+def check_pp(line, depth, brothers, macros, unkowns):
+	m = re.match("#\s*(ifdef|ifndef|if)\s*(.*)$", line)
+	if m:
+		f = check_ppif(m.group(1), m.group(2), macros, unkowns)
+		depth.append(f)
+		brothers.append([])
+		return all( x != 0 for x in depth ) and f == -1
+	m = re.match("#\s*elif(.*)$", line)
+	if m:
+		brother = brothers[-1]
+		brother.append(depth[-1])
+		f = 0
+		if not any( x == 1 for x in brother):
+			f = check_ppif("elif", m.group(1), macros, unkowns)
+		depth[-1] = f
+		return all( x != 0 for x in depth ) and any(x == -1 for x in brother)
+	m = re.match("#\s*else\s*$", line)
+	if m:
+		brother = brothers[-1]
+		f = depth[-1]
+		if f == 1 or any(x == 1 for x in brother):
+			f = 0
+		elif f == 0:
+			f = 1
+		depth[-1] = f
+		return all( x != 0 for x in depth ) and f == -1
+	if re.match("#\s*endif", line):
+		brother = brothers[-1]
+		f = depth.pop()
+		b1 = all( x != 0 for x in depth )
+		b2 = any(x == -1 for x in brother)
+		brothers.pop()
+		return b1 and (f == -1 or b2)
+	return len(depth) == 0 or all( x != 0 for x in depth )
+
+#
+#
+def preprocess(code, macros):
+	macros = { '__clang__':'1', '_LIBCPP_VERSION':'1101', 'NULL':'0'
+		  , '__linux__':'1', '__cplusplus':'201402'
+		  }
+	unkowns = [ '__has_include', '__has_feature' ]
+	depth = []
+	brother = []
+	dst = ""
+	for line in code.splitlines():
+		# c++ comment
+		if re.match('^//.*', line):
+			continue
+		# if/ifdef/ifndef/elif/endif
+		if check_pp(line, depth, brother, macros, unkowns):
+			# define
+			append_define(line, depth, macros, unkowns)
+			if len(line) > 0:
+				dst += line + "\n"
+		
+	#for k,v in sorted(macros.items()):
+	#	if v == None:
+	#		print(k)
+	#	else:
+	#		print(k + " " + v)
+	return dst
+
+#
 #
 def run_impl(code, options):
 	r = run_paiza(code, options)
@@ -157,28 +330,18 @@ def run(options):
 			output = "paizaio-toolong-sourcecode.cpp"
 			output_code(output, code, options.encoding)
 			print("source code -> " + output)
-		#try:
-		#	from pypreprocessor import pypreprocessor
-		#	try:
-		#		output = os.path.basename(filepath) + ".p"
-		#		code = re.sub('#define (.*)', r'# define \1', code)
-		#		output_code(output, code, options.encoding)
-		#		imp.acquire_lock()
-		#		#pypreprocessor.defines.append('IUTEST_VER')
-		#		pypreprocessor.input = output
-		#		#pypreprocessor.output = output + "p"
-		#		pypreprocessor.removeMeta = True
-		#		pypreprocessor.parse()
-		#		imp.release_lock()
-		#		file = file_open(output, 'r', options.encoding)
-		#		code = file.read()
-		#		file.close()
-		#		run_impl(code, options)
-		#	except Exception, e:
-		#		print(e)
-		#		raise
-		#except:
-		#	sys.exit(1)
+		try:
+			output = os.path.basename(filepath) + ".p"
+			macros = { '__clnag__':'1', '_LIBCPP_VERSION':'1101', 'NULL':'0' }
+			code = preprocess(code, macros)
+			output_code("paizaio-sourcecode.cpp", code, options.encoding)
+			run_impl(code, options)
+		except paizaio.TooLongException, e:
+			print(e)
+			sys.exit(1)
+		except Exception, e:
+			print(e)
+			raise
 	except:
 		raise
 
