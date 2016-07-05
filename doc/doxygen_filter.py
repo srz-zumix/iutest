48f8c0b8ccd4477a1e991465e3ef4fad650eaf26
srz-zumix <zumix.cpp@gmail.com>
2016-07-06 08:45:15 +0900

    tab to space update r1176

--- doc/doxygen_filter.py
+++ doc/doxygen_filter.py
@@ -3,7 +3,7 @@
 #
 # doxygen_filter.py
 #
-# Copyright (C) 2015, Takazumi Shirayanagi
+# Copyright (C) 2015-2016, Takazumi Shirayanagi
 # This software is released under the new BSD License,
 # see LICENSE
 #
@@ -14,21 +14,21 @@ import os
 
 # main
 def main():
-	path = sys.argv[1]
-	f = open(path)
-	n = 0
-	fname = os.path.basename(path)
-	if fname == "iutest_config.hpp":
-		n = 2
-	if f:
-		for line in f:
-			sys.stdout.write(line)
-			if n < 2:
-				if line.find("//=========================================================") != -1:
-					n += 1
-					if n == 2:
-						print('#include "iutest_config.hpp"')
-	f.close()
+    path = sys.argv[1]
+    f = open(path)
+    n = 0
+    fname = os.path.basename(path)
+    if fname == "iutest_config.hpp":
+        n = 2
+    if f:
+        for line in f:
+            sys.stdout.write(line)
+            if n < 2:
+                if line.find("//=========================================================") != -1:
+                    n += 1
+                    if n == 2:
+                        print('#include "iutest_config.hpp"')
+    f.close()
 
 if __name__ == '__main__':
-	main()
+    main()
