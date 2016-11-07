24661395fa10ba98c0cda46c2f6019dec092d078
srz-zumix <zumix.cpp@gmail.com>
2016-11-08 08:24:05 +0900

    add teest

---
+++ test/port_tests.cpp
@@ -0,0 +1,44 @@
+﻿//======================================================================
+//-----------------------------------------------------------------------
+/**
+ * @file        port_tests.cpp
+ * @brief       iutest portable function test
+ *
+ * @author      t.shirayanagi
+ * @par         copyright
+ * Copyright (C) 2016, Takazumi Shirayanagi\n
+ * The new BSD License is applied to this software.
+ * see LICENSE
+*/
+//-----------------------------------------------------------------------
+//======================================================================
+
+//======================================================================
+// include
+#include "iutest.hpp"
+
+
+IUTEST(PortableTest, SetGetEnv)
+{
+    const char* env_name = "IUTEST_PORT_TEST_ENV";
+    if( ::iutest::internal::posix::SetEnv(env_name, "test", 0) == -1 ) IUTEST_SKIP();
+    IUTEST_ASSERT_STREQ("test", ::iutest::internal::posix::GetEnv(env_name));
+    
+    IUTEST_EXPECT_NE(-1, ::iutest::internal::posix::SetEnv(env_name, "hoge", 0));
+    IUTEST_ASSERT_STREQ("test", ::iutest::internal::posix::GetEnv(env_name));
+
+    IUTEST_EXPECT_NE(-1, ::iutest::internal::posix::SetEnv(env_name, "hoge", 1));
+    IUTEST_ASSERT_STREQ("hoge", ::iutest::internal::posix::GetEnv(env_name));
+}
+
+
+#ifdef UNICODE
+int wmain(int argc, wchar_t* argv[])
+#else
+int main(int argc, char* argv[])
+#endif
+{
+    IUTEST_INIT(&argc, argv);
+    return IUTEST_RUN_ALL_TESTS();
+}
+
