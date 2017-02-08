4f3bf35a2b406f4aafb7f83f662de2bbe31a5f24
srz-zumix <zumix.cpp@gmail.com>
2017-02-09 08:23:09 +0900

    v1.16.1

--- include/iutest_ver.hpp
+++ include/iutest_ver.hpp
@@ -17,11 +17,11 @@
 
 //======================================================================
 // define
-#define IUTEST_VER          0x01160007u //!< iutest version 1.16.0.7
+#define IUTEST_VER          0x01160100u //!< iutest version 1.16.1.0
 #define IUTEST_MAJORVER     0x01u       //!< Major Version
 #define IUTEST_MINORVER     0x16u       //!< Minor Version
-#define IUTEST_MICROVER     0x00u       //!< Micro Version
-#define IUTEST_REVISION     0x07u       //!< Revision
+#define IUTEST_MICROVER     0x01u       //!< Micro Version
+#define IUTEST_REVISION     0x00u       //!< Revision
 
 #define IUTEST_BUILD        IUTEST_MICROVER //!< @deprecated
 
