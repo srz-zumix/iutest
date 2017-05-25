7efd8ff5c8a9c358a7aca6e6730ffe72e2232f87
srz-zumix <zumix.cpp@gmail.com>
2017-05-26 08:55:49 +0900

    update wandbox.py

--- tools/wandbox/wandbox.py
+++ tools/wandbox/wandbox.py
@@ -3,6 +3,10 @@
 # wandbox.py
 #
 
+"""
+Wandbox API for Python
+"""
+
 import requests
 import json
 
@@ -11,6 +15,7 @@ import json
 #
 class Wandbox:
     """wandbox api class"""
+
     #api_url = 'http://melpon.org/wandbox/api'
     api_url = 'https://wandbox.org/api'
 
@@ -23,73 +28,120 @@ class Wandbox:
     def __exit__(self, exc_type, exc_value, traceback):
         return True
 
+    """
+    get compiler list
+    """
     @staticmethod
     def GetCompilerList():
-        r = requests.get(Wandbox.api_url + '/list.json')
-        r.raise_for_status()
-        return r.json()
+        response = requests.get(Wandbox.api_url + '/list.json')
+        response.raise_for_status()
+        return response.json()
 
+    """
+    get compiler list
+    .. deprecated:: 0.3.4
+    """
     def get_compiler_list(self):
         return Wandbox.GetCompilerList()
 
+    """
+    get wandbox permanet link
+    """
     @staticmethod
     def GetPermlink(link):
-        r = requests.get(Wandbox.api_url + '/permlink/' + link)
-        r.raise_for_status()
-        return r.json()
+        response = requests.get(Wandbox.api_url + '/permlink/' + link)
+        response.raise_for_status()
+        return response.json()
 
+    """
+    get wandbox permanet link
+    .. deprecated:: 0.3.4
+    """
     def get_permlink(self, link):
         return Wandbox.GetPermlink(link)
 
+    """
+    excute on wandbox
+    """
     def run(self):
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
         payload = json.dumps(self.parameter)
-        r = requests.post(self.api_url + '/compile.json', data=payload, headers=headers)
-        r.raise_for_status()
-        return r.json()
+        response = requests.post(self.api_url + '/compile.json', data=payload, headers=headers)
+        response.raise_for_status()
+        return response.json()
 
-    def code(self, str):
-        self.parameter.update({'code': str})
+    """
+    set main source code
+    """
+    def code(self, code):
+        self.parameter.update({'code': code})
 
-    def add_file(self, filename, str):
+    """
+    append file
+    """
+    def add_file(self, filename, code):
         if 'codes' in self.parameter:
-            self.parameter['codes'].append({'file': filename, 'code': str})
+            self.parameter['codes'].append({'file': filename, 'code': code})
         else:
-            self.parameter.update({'codes': [{'file': filename, 'code': str}]})
+            self.parameter.update({'codes': [{'file': filename, 'code': code}]})
 
-    def compiler(self, str):
-        self.parameter.update({'compiler': str})
+    """
+    set compiler name
+    """
+    def compiler(self, name):
+        self.parameter.update({'compiler': name})
 
-    def options(self, str):
-        self.parameter.update({'options': str})
+    """
+    set wandbox options
+    """
+    def options(self, options_str):
+        self.parameter.update({'options': options_str})
 
-    def stdin(self, str):
-        self.parameter.update({'stdin': str})
+    """
+    set stdin buffer
+    """
+    def stdin(self, input):
+        self.parameter.update({'stdin': input})
 
-    def compiler_options(self, str):
-        self.parameter.update({'compiler-option-raw': str})
+    """
+    set wandbox defined compiler options
+    """
+    def compiler_options(self, options_str):
+        self.parameter.update({'compiler-option-raw': options_str})
 
-    def add_compiler_options(self, str):
+    """
+    set compiler options
+    """
+    def add_compiler_options(self, options_str):
         if 'compiler-option-raw' not in self.parameter:
-            self.compiler_options(str)
+            self.compiler_options(options_str)
         else:
             option = self.parameter['compiler-option-raw']
             option += '\n'
-            option += str
+            option += options_str
             self.parameter.update({'compiler-option-raw': option})
 
-    def runtime_options(self, str):
-        self.parameter.update({'runtime-option-raw': str})
+    """
+    set runtime options
+    """
+    def runtime_options(self, options_str):
+        self.parameter.update({'runtime-option-raw': options_str})
 
-    def permanent_link(self, b):
-        self.parameter.update({'save': b})
+    """
+    wandbox permanet link to enable
+    """
+    def permanent_link(self, enable):
+        self.parameter.update({'save': enable})
 
+    """
+    dump parameters
+    """
     def dump(self):
         print(self.parameter)
 
-    def close(self):
-        self.reset()
-
+    """
+    reset parametes
+    """
     def reset(self):
         self.parameter = {'code': ''}
 
