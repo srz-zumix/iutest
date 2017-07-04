ae7cd8c4ac5b4617f50dec76df5ca2e12d12e420
srz-zumix <zumix.cpp@gmail.com>
2017-07-05 08:02:58 +0900

    update documents

--- docs/dev/build.bat
+++ docs/dev/build.bat
@@ -2,13 +2,13 @@
 
 SET PATH=c:\cygwin\bin;%PATH%
 SET LANG=ja_JP.SJIS
-bash -c "make --no-print-directory %*" > build_log.txt 2>&1
+bash -c "make --no-print-directory %*" > build.log 2>&1
 if errorlevel 1 (
-	sed -e "s/: [0-9]*:/: error:/g" build_log.txt | sed -e "s/:\([0-9]*\):/(\1):/g" | sed -e "s/\/cygdrive\/\([a-z]\)/\1:/g" | sed -e "s/.*IUTEST_PP.*//g" | sed -e "s/.*INCLUDE_PATH\?.*//g" | sed -e "s/.*already documented.*//g"
-	rem del /F build_log.txt
+	sed -e "s/: [0-9]*:/: error:/g" build.log | sed -e "s/:\([0-9]*\):/(\1):/g" | sed -e "s/\/cygdrive\/\([a-z]\)/\1:/g" | sed -e "s/.*IUTEST_PP.*//g" | sed -e "s/.*INCLUDE_PATH\?.*//g" | sed -e "s/.*already documented.*//g"
+	rem del /F build.log
 	exit /b 1
 ) else (
-	sed -e "s/: [0-9]*:/: error:/g" build_log.txt | sed -e "s/:\([0-9]*\):/(\1):/g" | sed -e "s/\/cygdrive\/\([a-z]\)/\1:/g"
-	del /F build_log.txt
+	sed -e "s/: [0-9]*:/: error:/g" build.log | sed -e "s/:\([0-9]*\):/(\1):/g" | sed -e "s/\/cygdrive\/\([a-z]\)/\1:/g"
+	del /F build.log
 	exit /b 0
 )
