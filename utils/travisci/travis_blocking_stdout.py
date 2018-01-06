e0eb190b74fd3fd1ca65b22cb35bea6b0e51def1
srz-zumix <zumix.cpp@gmail.com>
2018-01-06 23:26:27 +0900

    fix error

--- utils/travisci/travis_blocking_stdout.py
+++ utils/travisci/travis_blocking_stdout.py
@@ -1,4 +1,5 @@
-import fcntl, sys, os
+import fcntl, os
+from sys import stdout, stderr
 
 def to_blocking_mode(channel):
     flags = fcntl.fcntl(channel, fcntl.F_GETFL)
@@ -7,7 +8,7 @@ def to_blocking_mode(channel):
         return True
     return False
 
-if to_blocking_mode(sys.stdout):
-    stderr.writelines(["Reset STDOUT to blocking"])
-if to_blocking_mode(sys.stderr):
+if to_blocking_mode(stderr):
     stderr.writelines(["Reset STDERR to blocking"])
+if to_blocking_mode(stdout):
+    stderr.writelines(["Reset STDOUT to blocking"])
