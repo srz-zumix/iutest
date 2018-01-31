1f25ec62b9a1d07c4d64c957d6eb54595bcf18ef
srz-zumix <zumix.cpp@gmail.com>
2018-01-31 09:47:37 +0900

    split japanese test name sample

---
+++ samples/japanese.cpp
@@ -0,0 +1,36 @@
+﻿//======================================================================
+//-----------------------------------------------------------------------
+/**
+ * @file        japanese.cpp
+ * @brief       japanese test name
+ *
+ * @author      t.shirayanagi
+ * @par         copyright
+ * Copyright (C) 2018, Takazumi Shirayanagi\n
+ * This software is released under the new BSD License,
+ * see LICENSE
+*/
+//-----------------------------------------------------------------------
+//======================================================================
+#include "../include/iutest.hpp"
+
+/* ---------------------------------------------------
+ * 日本語テスト名
+*//*--------------------------------------------------*/
+#if IUTEST_HAS_TESTNAME_ALIAS_JP
+
+IUTEST_PRAGMA_MSC_WARN_PUSH()
+IUTEST_PRAGMA_MSC_WARN_DISABLE(4566)
+
+IUTEST(IUTEST_JAPANESE_NAME(テスト), IUTEST_JAPANESE_NAME(テスト))
+{
+}
+class JapaneseFixedTest : public ::iutest::Test {};
+
+IUTEST_F(IUTEST_JAPANESE_NAME_F(あいうえお, JapaneseFixedTest), IUTEST_JAPANESE_NAME(かきくけこ))
+{
+}
+
+IUTEST_PRAGMA_MSC_WARN_POP()
+
+#endif
--- samples/simple.cpp
+++ samples/simple.cpp
@@ -6,7 +6,7 @@
  *
  * @author      t.shirayanagi
  * @par         copyright
- * Copyright (C) 2014-2016, Takazumi Shirayanagi\n
+ * Copyright (C) 2014-2018, Takazumi Shirayanagi\n
  * This software is released under the new BSD License,
  * see LICENSE
 */
@@ -43,27 +43,6 @@ IUTEST(SkipTest, Skip)
 {
     IUTEST_SKIP() << "empty.";
 }
-
-/* ---------------------------------------------------
- * 日本語テスト名
-*//*--------------------------------------------------*/
-#if IUTEST_HAS_TESTNAME_ALIAS_JP
-
-IUTEST_PRAGMA_MSC_WARN_PUSH()
-IUTEST_PRAGMA_MSC_WARN_DISABLE(4566)
-
-IUTEST(IUTEST_JAPANESE_NAME(テスト), IUTEST_JAPANESE_NAME(テスト))
-{
-}
-class JapaneseFixedTest : public ::iutest::Test {};
-
-IUTEST_F(IUTEST_JAPANESE_NAME_F(あいうえお, JapaneseFixedTest), IUTEST_JAPANESE_NAME(かきくけこ))
-{
-}
-
-IUTEST_PRAGMA_MSC_WARN_POP()
-
-#endif
 
 #if IUTEST_HAS_ASSERTION_RETURN
 
