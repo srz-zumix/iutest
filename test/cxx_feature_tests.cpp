836cf53cb2484f0f32dd5779cf8783fef2ca1ca4
srz-zumix <zumix.cpp@gmail.com>
2018-02-18 21:07:34 +0900

    add test

---
+++ test/cxx_feature_tests.cpp
@@ -0,0 +1,69 @@
+﻿//======================================================================
+//-----------------------------------------------------------------------
+/**
+ * @file        cxx_feature_tests.cpp
+ * @brief       iutest c++ feature test
+ *
+ * @author      t.shirayanagi
+ * @par         copyright
+ * Copyright (C) 2018, Takazumi Shirayanagi\n
+ * This software is released under the new BSD License,
+ * see LICENSE
+*/
+//-----------------------------------------------------------------------
+//======================================================================
+
+//======================================================================
+// include
+#include "../include/gtest/iutest_spi_switch.hpp"
+
+#if IUTEST_HAS_NOEXCEPT_FUNCTION_TYPE
+
+namespace
+{
+
+int f1()
+{
+    return 1;
+}
+int n1() noexcept
+{
+    return 1;
+}
+int f2()
+{
+    return 2;
+}
+int n2() noexcept
+{
+    return 2;
+}
+
+}
+
+IUTEST(NoexceptFounctionType, Assert)
+{
+    IUTEST_EXPECT_EQ(f1(), n1());
+    IUTEST_EXPECT_LE(f1(), n1());
+    IUTEST_EXPECT_GE(f1(), n1());
+    IUTEST_EXPECT_LT(f1(), n2());
+    IUTEST_EXPECT_GT(f2(), n1());
+    IUTEST_EXPECT_NE(f1(), n2());
+}
+
+IUTEST(NoexceptFounctionType, ExpressionAssert)
+{
+    IUTEST_EXPECT(f1() == n1());
+}
+
+#endif
+
+#ifdef UNICODE
+int wmain(int argc, wchar_t* argv[])
+#else
+int main(int argc, char* argv[])
+#endif
+{
+    IUTEST_INIT(&argc, argv);
+    return IUTEST_RUN_ALL_TESTS();
+}
