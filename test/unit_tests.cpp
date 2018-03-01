837ee77a541e638f759d343aff9e3dc10936dda0
srz-zumix <zumix.cpp@gmail.com>
2018-03-01 10:10:57 +0900

    update test

--- test/unit_tests.cpp
+++ test/unit_tests.cpp
@@ -171,7 +171,7 @@ IUTEST(UnitTest, GetEnvironmentVariable)
 IUTEST(StdFileUnitTest, AppendOpenedFileSize)
 {
     ::iutest::StdioFile file;
-    ::iutest::internal::FilePath filename("unit_tests.cpp");
+    ::iutest::internal::FilePath filename(__FILE__);
     IUTEST_ASSUME_TRUE( filename.FileOrDirectoryExists() );
     IUTEST_ASSERT_TRUE( file.Open(filename.c_str(), iutest::IFile::OpenAppend) );
     IUTEST_ASSERT_LT(0u, file.GetSize());
