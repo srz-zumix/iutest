9d88b8dfac075f8b949492793343b61f2e368c06
srz-zumix <zumix.cpp@gmail.com>
2018-03-16 22:15:28 +0900

    fix bug

--- include/iutest_printers.hpp
+++ include/iutest_printers.hpp
@@ -6,7 +6,7 @@
  *
  * @author      t.shirayanagi
  * @par         copyright
- * Copyright (C) 2011-2017, Takazumi Shirayanagi\n
+ * Copyright (C) 2011-2018, Takazumi Shirayanagi\n
  * This software is released under the new BSD License,
  * see LICENSE
 */
@@ -191,13 +191,13 @@ inline void DefaultPrintTo(IsContainerHelper::no_t
 
 template<typename T>
 inline void DefaultPtrPrintTo(T* ptr, iu_ostream* os
-    , typename iutest_type_traits::enable_if_t< is_convertible<T*, const void*> >::type*& = iutest_type_traits::enabler::value)
+    , typename iutest_type_traits::enable_if_t< iutest_type_traits::is_convertible<T*, const void*> >::type*& = iutest_type_traits::enabler::value)
 {
     *os << ptr;
 }
 template<typename T>
 inline void DefaultPtrPrintTo(T* ptr, iu_ostream* os
-    , typename iutest_type_traits::disable_if_t< is_convertible<T*, const void*> >::type*& = iutest_type_traits::enabler::value)
+    , typename iutest_type_traits::disable_if_t< iutest_type_traits::is_convertible<T*, const void*> >::type*& = iutest_type_traits::enabler::value)
 {
     *os << reinterpret_cast<const void*>(reinterpret_cast<type_least_t<8>::UInt>(ptr));
 }
