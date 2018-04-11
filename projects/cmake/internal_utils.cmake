1d6ee5fddfb84c0189b485eb15fd56d89e37eceb
srz-zumix <zumix.cpp@gmail.com>
2018-04-11 23:53:04 +0900

    add cxxflag log

--- projects/cmake/internal_utils.cmake
+++ projects/cmake/internal_utils.cmake
@@ -19,6 +19,10 @@ macro(fix_default_compiler_settings_)
       endif()
     endforeach()
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /WX")
+    foreach (flag_var
+             CMAKE_CXX_FLAGS)
+      message(STATUS "${flag_var}=${${flag_var}}")
+    endforeach()
   endif()
 endmacro()
 
