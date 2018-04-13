11369caf1db269a6b4614e4679d4ec12ceb5762f
srz-zumix <zumix.cpp@gmail.com>
2018-04-13 10:01:58 +0900

    revert pragma position

--- include/iutest_package.hpp
+++ include/iutest_package.hpp
@@ -66,7 +66,10 @@
 #define IIUT_PACKAGE_PARENT_NAMESPACE_(name)                \
     class iuTest_TestCaseParentPackage;                     \
     __if_not_exists(name::iuTest_GetTestCaseParentPackageName) {    \
+        IUTEST_PRAGMA_MSC_WARN_PUSH()                       \
+        IUTEST_PRAGMA_MSC_WARN_DISABLE(4505)                \
         IIUT_PACKAGE_DECL_PARENT_NAME_FUNC(name)            \
+        IUTEST_PRAGMA_MSC_WARN_POP()                        \
     }
 
 #else
@@ -89,13 +92,10 @@
 #endif
 
 #define IIUT_PACKAGE_(name)                                 \
-    IUTEST_PRAGMA_MSC_WARN_PUSH()                           \
-    IUTEST_PRAGMA_MSC_WARN_DISABLE(4505)                    \
     namespace name {                                        \
         IIUT_PACKAGE_CURRENT_NAMESPACE_(name)               \
         IIUT_PACKAGE_PARENT_NAMESPACE_(name)                \
     }                                                       \
-    IUTEST_PRAGMA_MSC_WARN_POP()                            \
     namespace name
 
 #else
