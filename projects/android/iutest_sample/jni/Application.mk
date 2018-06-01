6d7b8d6126f40ab7e80519309d58ba489eb4a5cd
srz_zumix <zumix.cpp@gmail.com>
2018-06-01 12:31:44 +0900

    workaround android build error #59

--- projects/android/iutest_sample/jni/Application.mk
+++ projects/android/iutest_sample/jni/Application.mk
@@ -1,6 +1,6 @@
 APP_OPTIM		:= release
 APP_ABI			:= armeabi-v7a x86
 APP_PLATFORM    := android-14
-APP_STL 		:= c++_static
+APP_STL 		:= gnustl_static
 #APP_STL := stlport_static
 APP_CPPFLAGS := -frtti -fexceptions
