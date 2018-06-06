c72aeae6bf65928d2dee8194c2434442392a57e4
srz-zumix <zumix.cpp@gmail.com>
2018-06-06 09:49:22 +0900

    update crlf check script

--- .ci/check-crlf.sh
+++ .ci/check-crlf.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-GREP_OPT="dont_match_dummy_string"
-for i in `grep -oP '(?<=\*\.)[\S]+(?=.*eol=crlf)' ../.gitattributes`
+
+if git rev-parse --ls-include-work-tree > /dev/nul 2>&1; then
+    cd `pwd`/`git rev-parse --show-cdup`
+fi
+GREP_OPT=dont_match_dummy_string
+for i in `grep -oP '(?<=\*\.)[\S]+(?=.*eol=crlf)' .gitattributes`
 do
     GREP_OPT="$i|${GREP_OPT}"
 done
