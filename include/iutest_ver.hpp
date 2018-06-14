e46299b73486d7a13816dacad4742fd887e19887
srz-zumix <zumix.cpp@gmail.com>
2018-06-14 10:12:07 +0900

    support GTEST_LATEST

--- include/iutest_ver.hpp
+++ include/iutest_ver.hpp
@@ -17,11 +17,11 @@
 
 //======================================================================
 // define
-#define IUTEST_VER          0x01160303u //!< iutest version 1.16.3.3
+#define IUTEST_VER          0x01160304u //!< iutest version 1.16.3.4
 #define IUTEST_MAJORVER     0x01u       //!< Major Version
 #define IUTEST_MINORVER     0x16u       //!< Minor Version
 #define IUTEST_MICROVER     0x03u       //!< Micro Version
-#define IUTEST_REVISION     0x03u       //!< Revision
+#define IUTEST_REVISION     0x04u       //!< Revision
 
 #define IUTEST_BUILD        IUTEST_MICROVER //!< @deprecated
 
