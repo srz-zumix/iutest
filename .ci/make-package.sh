885b2413fe4463c1aa533046dab337d2efbe19ff
srz-zumix <zumix.cpp@gmail.com>
2018-06-16 22:24:31 +0900

    add make package script

---
+++ .ci/make-package.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if git rev-parse --ls-include-work-tree > /dev/null 2>&1; then
+    cd `pwd`/`git rev-parse --show-cdup`
+fi
+
+if [ -z $RELEASE_VERSION ]; then
+    BRANCH_NAME=`echo $(\git symbolic-ref --short HEAD) | sed s:/:-:g`
+    RELEASE_VERSION=$BRANCH_NAME
+fi
+
+echo $RELEASE_VERSION
+PACKAGE_NAME=iutest-$RELEASE_VERSION
+
+# make fused
+make -C tools/fused
+git stash -u
+
+# packaging
+git archive --format=tar.gz 'stash@{0}' > $PACKAGE_NAME.tar.gz
+git archive --format=zip    'stash@{0}' > $PACKAGE_NAME.zip
