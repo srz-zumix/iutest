db299232197f85504e3e30e6a1f92a29b6b4049e
srz-zumix <zumix.cpp@gmail.com>
2018-07-25 23:17:18 +0900

    add xcode cxx detect util

---
+++ utils/xcode.mk
@@ -0,0 +1,33 @@
+#
+# xcode.mk
+#
+# detect xcode compiler
+#
+# Copyright (C) 2018, Takazumi Shirayanagi
+# This software is released under the new BSD License,
+# see LICENSE
+#
+
+APPLE_CXXVERSION:=$(shell $(CXX) --version | grep -o Apple)
+
+ifeq ($(APPLE_CXXVERSION),Apple)
+
+CXX_APPLE_CLANG=1
+CXX_NAME=clang++
+CLANGVERSION:=$(shell $(CXX) --version | grep -o "version\s[0-9]*\.[0-9]*")
+
+dot:=.
+empty:=
+space:=$(empty) $(empty)
+CLANGVERSION:=$(subst -,$(dot), $(CLANGVERSION))
+CLANGVERSION:=$(subst $(dot),$(space), $(CLANGVERSION))
+
+ifeq ($(words $(CLANGVERSION)), 3)
+CLANGMAJOR:=3
+CLANGMINOR:=$(word 2, $(CLANGVERSION))
+else
+CLANGMAJOR:=0
+CLANGMINOR:=0
+endif
+
+endif
