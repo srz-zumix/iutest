87f81b3dc4011398350d2e8c4fd06980d4eff1b0
srz-zumix <zumix.cpp@gmail.com>
2018-08-25 08:19:14 +0900

    change init timing

--- include/iutest_case.hpp
+++ include/iutest_case.hpp
@@ -42,14 +42,14 @@ protected:
      * @param [in]  teardown        = テスト事後実行関数
     */
     TestCase(const char* testcase_name, TestTypeId id, SetUpMethod setup, TearDownMethod teardown)
-    : m_setup(setup), m_teardown(teardown)
+    : m_testcase_name(testcase_name)
+    , m_setup(setup), m_teardown(teardown)
     , m_id(id), m_disable_num(0)
     , m_should_run_num(0)
     , m_elapsedmsec(0)
     , m_start_timestamp(0)
     , m_disable(false)
     {
-        m_testcase_name = testcase_name;
         if( detail::IsStringForwardMatching(testcase_name, "DISABLED_")
             || (strstr(testcase_name, "/DISABLED_") != NULL) )
         {
