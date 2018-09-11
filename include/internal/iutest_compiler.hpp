5fee7b95b0a86caef9102c9fe2da1c65eba0c760
srz-zumix <zumix.cpp@gmail.com>
2018-09-12 01:38:19 +0900

    fix msvc error #83

--- include/internal/iutest_compiler.hpp
+++ include/internal/iutest_compiler.hpp
@@ -884,7 +884,7 @@
 #if !defined(IUTEST_HAS_INT128)
 #  if defined(__SIZEOF_INT128__) && __SIZEOF_INT128__ == 16
 #    define IUTEST_HAS_INT128   1
-#  elif defined(_MSC_VER)
+#  elif defined(_MSC_VER) && defined(_M_X64)
 #    define IUTEST_HAS_INT128   1
 #  endif
 #endif
