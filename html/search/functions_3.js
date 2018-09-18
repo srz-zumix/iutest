b772930462f01f552e15bcf6bcf78d72e048cdfd
srz-zumix <zumix.cpp@gmail.com>
2018-09-18 14:11:47 +0000

    update gh-pages git@955ae42139f77541334fa7739798293496610655

--- html/search/functions_3.js
+++ html/search/functions_3.js
@@ -5,5 +5,6 @@ var searchData=
   ['defaultxmlgeneratorlistener',['DefaultXmlGeneratorListener',['../db/da9/classiutest_1_1_default_xml_generator_listener.html#ac2f550a886693abdb68475a78448a27b',1,'iutest::DefaultXmlGeneratorListener']]],
   ['disabled_5ftest_5fcount',['disabled_test_count',['../df/d67/classiutest_1_1_test_case.html#acbb9064e08224f72adc8dfc9b76163cb',1,'iutest::TestCase::disabled_test_count()'],['../de/d13/classiutest_1_1_unit_test.html#adfd2b2b25b2851a739ebbbb09d93f3eb',1,'iutest::UnitTest::disabled_test_count()']]],
   ['doubleeq',['DoubleEq',['../d4/d14/group___m_a_t_c_h_e_r_s.html#gae93e07104c20e4a648fb059da6bb601e',1,'iutest::matchers']]],
-  ['doublele',['DoubleLE',['../d3/dda/namespaceiutest.html#a8ceceea2f7b45e7f7e337c01387e3d97',1,'iutest']]]
+  ['doublele',['DoubleLE',['../d3/dda/namespaceiutest.html#a8ceceea2f7b45e7f7e337c01387e3d97',1,'iutest']]],
+  ['doublenear',['DoubleNear',['../d4/d14/group___m_a_t_c_h_e_r_s.html#gaa437fdfa944b50a411361bf653d2444f',1,'iutest::matchers']]]
 ];
--- html/search/functions_d.js
+++ html/search/functions_d.js
@@ -2,9 +2,13 @@ var searchData=
 [
   ['name',['name',['../df/d67/classiutest_1_1_test_case.html#a2faa0f8d2c9c5dea207524bf99799f00',1,'iutest::TestCase::name()'],['../d8/d3d/classiutest_1_1_test_info.html#acb83951703c7bab66c69de0f7c0e95e3',1,'iutest::TestInfo::name()']]],
   ['nansensitivealmostequals',['NanSensitiveAlmostEquals',['../d3/d81/classiutest_1_1floating__point.html#a1c3e745f3d35b5c9f9be13904627ac99',1,'iutest::floating_point']]],
+  ['nansensitivealmostnear',['NanSensitiveAlmostNear',['../d3/d81/classiutest_1_1floating__point.html#a888b4a9bf197a86e6b7e64062769e1af',1,'iutest::floating_point']]],
   ['nansensitivedoubleeq',['NanSensitiveDoubleEq',['../d4/d14/group___m_a_t_c_h_e_r_s.html#gab6781e0013e20fd03c26c8549ceb38be',1,'iutest::matchers']]],
+  ['nansensitivedoublenear',['NanSensitiveDoubleNear',['../d4/d14/group___m_a_t_c_h_e_r_s.html#ga09c0c188650b32cae64d8aa8e6fe6736',1,'iutest::matchers']]],
   ['nansensitivefloateq',['NanSensitiveFloatEq',['../d4/d14/group___m_a_t_c_h_e_r_s.html#gaa1dbfedcbf810557d5a0c354fb45eeb1',1,'iutest::matchers']]],
   ['nansensitivefloatingpointeq',['NanSensitiveFloatingPointEq',['../d4/d14/group___m_a_t_c_h_e_r_s.html#ga55d4f87b218eb0e76e0ba5b15809d9ab',1,'iutest::matchers']]],
+  ['nansensitivefloatingpointnear',['NanSensitiveFloatingPointNear',['../d4/d14/group___m_a_t_c_h_e_r_s.html#ga94946d97575e0b94e96a688ffd1ef7c8',1,'iutest::matchers']]],
+  ['nansensitivefloatnear',['NanSensitiveFloatNear',['../d4/d14/group___m_a_t_c_h_e_r_s.html#ga8fc54ce7043c2eae2e98ef1e50c9bd0c',1,'iutest::matchers']]],
   ['ne',['Ne',['../d4/d14/group___m_a_t_c_h_e_r_s.html#ga7f7abedb821d6cdd3ea6297c5b2b89c9',1,'iutest::matchers::Ne(const T &amp;expected)'],['../d4/d14/group___m_a_t_c_h_e_r_s.html#gabb443c6232361be33e0ae183d2d91e31',1,'iutest::matchers::Ne()']]],
   ['ninf',['NINF',['../d3/d81/classiutest_1_1floating__point.html#ae8c1c5b46b42e78b4134a1ef01b39d4f',1,'iutest::floating_point']]],
   ['nnan',['NNAN',['../d3/d81/classiutest_1_1floating__point.html#a1d57c4e43ec005d88ac0ca0bbc799c29',1,'iutest::floating_point']]],
