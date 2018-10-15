b8e43f83e741a5a07394b468dd29fbeb92d95717
srz-zumix <zumix.cpp@gmail.com>
2018-10-16 08:44:13 +0900

    update android sample

--- projects/android/iutest_sample/jni/Application.mk
+++ projects/android/iutest_sample/jni/Application.mk
@@ -1,6 +1,5 @@
 APP_OPTIM		:= release
-APP_ABI			:= armeabi-v7a x86
-APP_PLATFORM    := android-14
-APP_STL 		:= gnustl_static
-#APP_STL := stlport_static
+APP_ABI			:= arm64-v8a x86
+APP_PLATFORM    := android-21
+APP_STL 		:= c++_static
 APP_CPPFLAGS := -frtti -fexceptions
