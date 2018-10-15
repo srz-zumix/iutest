3b9d78079640b82a70dfb918eebc6b341683feba
srz-zumix <zumix.cpp@gmail.com>
2018-10-16 08:44:13 +0900

    android sample setting

--- projects/android/iutest_sample/jni/Application.mk
+++ projects/android/iutest_sample/jni/Application.mk
@@ -1,6 +1,5 @@
 APP_OPTIM		:= release
-APP_ABI			:= armeabi-v7a x86
-APP_PLATFORM    := android-14
+APP_ABI			:= arm64-v8a x86
+APP_PLATFORM    := android-26
 APP_STL 		:= gnustl_static
-#APP_STL := stlport_static
 APP_CPPFLAGS := -frtti -fexceptions
