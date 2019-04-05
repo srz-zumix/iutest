57bb27198c3749b8d61be9e9e608b47beda65a5e
srz_zumix <zumix.cpp@gmail.com>
2019-04-05 10:13:51 +0900

    fix order

--- include/internal/iutest_stdlib.hpp
+++ include/internal/iutest_stdlib.hpp
@@ -6,7 +6,7 @@
  *
  * @author      t.shirayanagi
  * @par         copyright
- * Copyright (C) 2012-2018, Takazumi Shirayanagi\n
+ * Copyright (C) 2012-2019, Takazumi Shirayanagi\n
  * This software is released under the new BSD License,
  * see LICENSE
 */
@@ -386,13 +386,13 @@
 #if !defined(IUTEST_HAS_CXX_HDR_CODECVT)
 #  define IUTEST_HAS_CXX_HDR_CODECVT    0
 #endif
-//! has cuchar
-#if !defined(IUTEST_HAS_CXX_HDR_CUCHAR)
-#  define IUTEST_HAS_CXX_HDR_CUCHAR     0
-#endif
 //! has cstdint header
 #if !defined(IUTEST_HAS_CXX_HDR_CSTDINT)
 #  define IUTEST_HAS_CXX_HDR_CSTDINT    0
+#endif
+//! has cuchar
+#if !defined(IUTEST_HAS_CXX_HDR_CUCHAR)
+#  define IUTEST_HAS_CXX_HDR_CUCHAR     0
 #endif
 //! has filesystem header
 #if !defined(IUTEST_HAS_CXX_HDR_FILESYSTEM)
