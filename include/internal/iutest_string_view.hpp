9b4863e87ab0f9f07dbb1f7c55f290e63b3ad963
srz_zumix <zumix.cpp@gmail.com>
2019-04-12 10:20:21 +0900

    fix nullable string_view

--- include/internal/iutest_string_view.hpp
+++ include/internal/iutest_string_view.hpp
@@ -385,39 +385,46 @@ template<typename CharT, typename Traits = ::std::char_traits<CharT> >
 class iu_nullable_basic_string_view : public iu_basic_string_view<CharT, Traits>
 {
 public:
+    typedef iu_basic_string_view<CharT, Traits> _Mybase;
+    typedef Traits  traits_type;
+    typedef CharT   value_type;
+    typedef value_type* pointer;
+    typedef const value_type* const_pointer;
+    typedef size_t size_type;
+public:
     IUTEST_CXX_CONSTEXPR iu_nullable_basic_string_view() IUTEST_CXX_NOEXCEPT_SPEC IUTEST_CXX_DEFAULT_FUNCTION;
 #if IUTEST_HAS_NULLPTR
     IUTEST_CXX_CONSTEXPR iu_nullable_basic_string_view(::std::nullptr_t)
-        : iu_basic_string_view(IUTEST_NULLPTR, 0)
+        : _Mybase(IUTEST_NULLPTR, 0)
     {
     }
 #endif
     IUTEST_CXX_CONSTEXPR iu_nullable_basic_string_view(const_pointer str)
-        : iu_basic_string_view(str, str ? traits_type::length(str) : 0)
+        : _Mybase(str, str ? traits_type::length(str) : 0)
     {
     }
     IUTEST_CXX_CONSTEXPR iu_nullable_basic_string_view(const_pointer str, size_type len)
-        : iu_basic_string_view(str, len)
+        : _Mybase(str, len)
     {
     }
 #if IUTEST_HAS_DEFAULT_FUNCTIONS
     IUTEST_CXX_CONSTEXPR iu_nullable_basic_string_view(const iu_nullable_basic_string_view&) IUTEST_CXX_NOEXCEPT_SPEC = default;
 #else
     IUTEST_CXX_CONSTEXPR iu_nullable_basic_string_view(const iu_nullable_basic_string_view& rhs) IUTEST_CXX_NOEXCEPT_SPEC
-        : iu_basic_string_view(rhs)
+        : _Mybase(rhs)
     {
     }
 #endif
 
     template<size_t N>
     iu_nullable_basic_string_view(value_type(&str)[N])
-        : iu_basic_string_view(str, N)
+        : _Mybase(str, N)
     {
     }
 
     template<typename Allocator>
     iu_nullable_basic_string_view(const ::std::basic_string<value_type, traits_type, Allocator>& str)
-        : iu_basic_string_view(str.data(), str.length())
+        : _Mybase(str.data(), str.length())
     {
     }
 };
