bbc240779e42c8f4ba4b09ab8992895d11a3e433
srz_zumix <zumix.cpp@gmail.com>
2019-04-19 10:03:18 +0900

    include stdint

--- include/internal/iutest_stdlib.hpp
+++ include/internal/iutest_stdlib.hpp
@@ -24,6 +24,9 @@
 #  include <cstdio>
 #  define __STRICT_ANSI__
 #endif
+#if !defined(_MSC_VER)
+#  include <stdint.h>
+#endif
 #include <cstdlib>
 
 //======================================================================
