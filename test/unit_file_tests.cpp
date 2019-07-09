f215448d3a921575a32767c12df6c80f705c70f1
srz_zumix <zumix.cpp@gmail.com>
2019-07-09 10:02:11 +0900

    fix enum type

--- test/unit_file_tests.cpp
+++ test/unit_file_tests.cpp
@@ -58,7 +58,7 @@ IUTEST_F(FileSystemTest, FileSize64bit)
     IUTEST_ASSUME_EQ(0x100000000ull, ::std::filesystem::file_size(largefile));
 
     ::iutest::StdioFile file;
-    IUTEST_ASSERT_TRUE( file.Open(largefile, iutest::IFile::Open) );
+    IUTEST_ASSERT_TRUE( file.Open(largefile, iutest::IFile::OpenRead) );
     IUTEST_EXPECT_EQ(0x100000000ull, file.GetSize());
 }
 
