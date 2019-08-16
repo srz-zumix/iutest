2d8d7fea5f18274f98462224bb3891c1a3925881
srz_zumix <zumix.cpp@gmail.com>
2019-08-16 10:31:13 +0900

    update version

--- include/iutest_ver.hpp
+++ include/iutest_ver.hpp
@@ -17,11 +17,11 @@
 
 //======================================================================
 // define
-#define IUTEST_VER          0x01169924u //!< iutest version 1.16.99.24
+#define IUTEST_VER          0x01169925u //!< iutest version 1.16.99.25
 #define IUTEST_MAJORVER     0x01u       //!< Major Version
 #define IUTEST_MINORVER     0x16u       //!< Minor Version
 #define IUTEST_MICROVER     0x99u       //!< Micro Version
-#define IUTEST_REVISION     0x24u       //!< Revision
+#define IUTEST_REVISION     0x25u       //!< Revision
 
 #define IUTEST_BUILD        IUTEST_MICROVER //!< @deprecated
 
@@ -123,7 +123,7 @@ modification, are permitted provided that the following conditions are met: \n
  <ul>
   <li>v1.17.0.0
     <ul>
-      <li></li>
+      <li>FreeBSD 対応</li>
       <li>ALMOST_EQ アサーション を追加</li>
       <li>--iutest_locale_ctype オプションを追加</li>
       <li>setlocale(LC_CTYPE, IUTEST_FLAG(locale_ctype)) をテスト実行開始時に行うように修正</li>
