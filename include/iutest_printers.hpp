49d3ed4e69b31be0cca53c1982d32b2966356770
srz_zumix <zumix.cpp@gmail.com>
2019-09-30 10:01:40 +0900

    test

--- include/iutest_printers.hpp
+++ include/iutest_printers.hpp
@@ -394,9 +394,15 @@ inline ::std::string FileSystemFileTypeToString(const ::std::filesystem::file_ty
     }
     return PrintToString(static_cast<int>(value));
 }
-inline void PrintTo(const ::std::filesystem::path& value, iu_ostream* os)
+
+struct fs_path_carrier
 {
-    *os << value.generic_string();
+    fs_path_carrier(const ::std::filesystem::path& path) : m_path(path) {}
+    const ::std::filesystem::path& m_path;
+};
+inline void PrintTo(fs_path_carrier value, iu_ostream* os)
+{
+    *os << value.m_path.generic_string();
 }
 inline void PrintTo(const ::std::filesystem::file_type& value, iu_ostream* os)
 {
