c58f819a5e396d9bff21d05d412948ac4a8e7232
srz_zumix <zumix.cpp@gmail.com>
2020-03-09 21:34:23 +0900

    update editorconfig

--- projects/make/build.bat
+++ projects/make/build.bat
@@ -3,11 +3,11 @@
 SET LANG=ja_JP.SJIS
 bash -c "make --no-print-directory %*" 2> build_log.txt
 if errorlevel 1 (
-	sed -e "s/:\([0-9]*\):[0-9]*:/(\1):/g" build_log.txt | sed -e "s/\/cygdrive\/\([a-z]\)/\1:/g"
-	del /F build_log.txt
-	exit /b 1
+    sed -e "s/:\([0-9]*\):[0-9]*:/(\1):/g" build_log.txt | sed -e "s/\/cygdrive\/\([a-z]\)/\1:/g"
+    del /F build_log.txt
+    exit /b 1
 ) else (
-	sed -e "s/:\([0-9]*\):[0-9]*:/(\1):/g" build_log.txt | sed -e "s/\/cygdrive\/\([a-z]\)/\1:/g"
-	del /F build_log.txt
-	exit /b 0
+    sed -e "s/:\([0-9]*\):[0-9]*:/(\1):/g" build_log.txt | sed -e "s/\/cygdrive\/\([a-z]\)/\1:/g"
+    del /F build_log.txt
+    exit /b 0
 )
