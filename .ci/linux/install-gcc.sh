70c3641c5b2364eb47902cf61f0bb055b228ed17
srz_zumix <zumix.cpp@gmail.com>
2020-04-12 15:53:36 +0900

    debug

--- .ci/linux/install-gcc.sh
+++ .ci/linux/install-gcc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e
+set -ex
 
 if [ -z ${GCC_VERSION+x} ]; then
   echo "GCC_VERSION is empty. skip install gcc..."
@@ -8,7 +8,7 @@ if [ -z ${GCC_VERSION+x} ]; then
 fi
 
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt-get -y --allow-unauthenticated update
+sudo apt-get -y update
 sudo apt-get -qq install g++-${GCC_VERSION}
 sudo update-alternatives --install /usr/bin/gcc gcc \
   /usr/bin/gcc-${GCC_VERSION} 90 \
