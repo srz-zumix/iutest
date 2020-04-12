5556899f406911dd139c53da0f4e21ab0233618e
srz_zumix <zumix.cpp@gmail.com>
2020-04-12 15:34:51 +0900

    sudo

--- .ci/linux/install-clang.sh
+++ .ci/linux/install-clang.sh
@@ -3,23 +3,23 @@
 set -e
 
 if [ -z ${CLANG_VERSION+x} ]; then
-  echo "CLANG_VERSOIN is empty. skip install clang..."
+  echo "CLANG_VERSION is empty. skip install clang..."
   return
 fi
 
 . /etc/os-release
 wget -qO - http://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-add-apt-repository -y 'deb http://apt.llvm.org/${VERSION_CODENAME}/ llvm-toolchain-${VERSION_CODENAME}-${CLANG_VERSION} main'
-apt-get -y --allow-unauthenticated update
-apt-get -qq install clang-${CLANG_VERSION}
+sudo add-apt-repository -y "deb http://apt.llvm.org/${VERSION_CODENAME}/ llvm-toolchain-${VERSION_CODENAME}-${CLANG_VERSION} main"
+sudo apt-get -y --allow-unauthenticated update
+sudo apt-get -qq install clang-${CLANG_VERSION}
 
 
 if $INSATLL_CLANG_TOOLS; then
-  apt-get -y --force-yes -qq install clang-$CLANG_VERSION clang-tools-$CLANG_VERSION
+  sudo apt-get -y --force-yes -qq install clang-$CLANG_VERSION clang-tools-$CLANG_VERSION
   CLANG_SLAVES="--slave /usr/bin/scan-build scan-build /usr/bin/scan-build-$CLANG_VERSION"
 fi
 
-update-alternatives --install /usr/bin/clang clang \
+sudo update-alternatives --install /usr/bin/clang clang \
   /usr/bin/clang-${CLANG_VERSION} 360 \
   --slave /usr/bin/clang++ clang++ /usr/bin/clang++-${CLANG_VERSION} \
   ${CLANG_SLAVES}
--- .ci/linux/install-gcc.sh
+++ .ci/linux/install-gcc.sh
@@ -7,9 +7,9 @@ if [ -z ${GCC_VERSION+x} ]; then
   return
 fi
 
-add-apt-repository -y ppa:ubuntu-toolchain-r/test
-apt-get -y --allow-unauthenticated update
-apt-get -qq install g++-${GCC_VERSION}
-update-alternatives --install /usr/bin/gcc gcc \
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get -y --allow-unauthenticated update
+sudo apt-get -qq install g++-${GCC_VERSION}
+sudo update-alternatives --install /usr/bin/gcc gcc \
   /usr/bin/gcc-${GCC_VERSION} 90 \
   --slave /usr/bin/g++ g++ /usr/bin/g++-${GCC_VERSION}
