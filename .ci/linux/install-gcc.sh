435a4c969c0f080c89b0aa96f35dc64e0d3f76f7
srz_zumix <zumix.cpp@gmail.com>
2020-04-12 20:35:05 +0900

    add key

--- .ci/linux/install-gcc.sh
+++ .ci/linux/install-gcc.sh
@@ -7,9 +7,10 @@ if [ -z ${GCC_VERSION+x} ]; then
   return
 fi
 
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test || true
-sudo apt-get -y update
-sudo apt-get -qq install g++-${GCC_VERSION}
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get -y --allow-unauthenticated update
+sudo apt-get -qq install "g++-${GCC_VERSION}"
 sudo update-alternatives --install /usr/bin/gcc gcc \
   /usr/bin/gcc-${GCC_VERSION} 90 \
   --slave /usr/bin/g++ g++ /usr/bin/g++-${GCC_VERSION}
