2bdda5a1f36a7e0cb3602545d8d168640a007ef7
srz_zumix <zumix.cpp@gmail.com>
2020-04-29 11:20:16 +0900

    update

--- .github/actions/update-ghpages/entrypoint.sh
+++ .github/actions/update-ghpages/entrypoint.sh
@@ -3,24 +3,12 @@
 set -exu
 
 export CI_COMMIT_ID="${GITHUB_SHA}"
-export PREVCOMMIT="${CI_COMMIT_ID}^"
 
 export DIRNAME_="${GITHUB_REF#refs/heads/}"
 export DIRNAME="${DIRNAME_#refs/tags/}"
 
-if [ "${DIRNAME}" = 'master' ]; then
-  git clone -b gh-pages --depth 1 git@github.com:srz-zumix/iutest.git gh-pages-temp
-  cd gh-pages-temp
-  git log -n -1
-  if `git log -n 1 | grep -q 'git@[0-9a-zA-Z]*'` ; then export PREVCOMMIT=`git log -n 1 | grep 'git@[0-9a-zA-Z]*' | head -1 | sed -e 's/.*git@\([0-9a-zA-Z]*\).*/\1/g'`; fi
-  cd ../
-  echo ${PREVCOMMIT}
-  export GH_PAGES_UPDATE=0
-  if ! git diff "$CI_COMMIT_ID:include" "$PREVCOMMIT:include" --exit-code --quiet; then export GH_PAGES_UPDATE=1; fi
-  if ! git diff "$CI_COMMIT_ID:docs" "$PREVCOMMIT:docs" --exit-code --quiet; then export GH_PAGES_UPDATE=1; fi
-else
-  export GH_PAGES_UPDATE=1
-  if [ ! "${DIRNAME}" =~ v.* ]; then
+if [ "${DIRNAME}" != 'master' ]; then
+  if [[ ! "${DIRNAME}" =~ v.* ]]; then
       DIRNAME=test
   fi
 fi
@@ -33,15 +21,15 @@ export PATH=$PATH:$PWD
 echo $PATH
 git clone -b gh-pages git@github.com:srz-zumix/iutest.git gh-pages
 
-OUTDIR=gh-pages/${DIRNAME}
+OUTDIR="gh-pages/${DIRNAME}"
 if [ -d "${OUTDIR}" ]; then
-  cd ${OUTDIR}
+  cd "${OUTDIR}"
   git rm -r *
   cd -
 fi
 
 make
-make gh-pages GHPAGES_DIR=${OUTDIR}
+make gh-pages GHPAGES_DIR="${OUTDIR}"
 
 cd gh-pages
 git config user.email "zumix.cpp@gmail.com"
@@ -51,7 +39,4 @@ git add --all
 #git commit -m $GIT_COMMIT_MESSAGE
 git commit -m "update gh-pages git@$CI_COMMIT_ID"
 
-echo "$GH_PAGES_UPDATE"
-if [ $GH_PAGES_UPDATE == 1 ]; then
-  git push
-fi
+git push
