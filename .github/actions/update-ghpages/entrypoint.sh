8cb250f8d5f54926b46ddd13b2446797d9453884
srz_zumix <zumix.cpp@gmail.com>
2020-04-29 11:10:25 +0900

    update

--- .github/actions/update-ghpages/entrypoint.sh
+++ .github/actions/update-ghpages/entrypoint.sh
@@ -2,37 +2,38 @@
 
 set -exu
 
-export CI_COMMIT_ID=${GITHUB_SHA}
-export PREVCOMMIT=${CI_COMMIT_ID}^
+export CI_COMMIT_ID="${GITHUB_SHA}"
+export PREVCOMMIT="${CI_COMMIT_ID}^"
 
-export DIRNAME_=${GITHUB_REF#refs/heads/}
-export DIRNAME=${DIRNAME_#refs/tags/}
+export DIRNAME_="${GITHUB_REF#refs/heads/}"
+export DIRNAME="${DIRNAME_#refs/tags/}"
 
-if [ ${DIRNAME} != 'master' ]; then
-  if [ ! ${DIRNAME} =~ v.* ]; then
+if [ "${DIRNAME}" = 'master' ]; then
+  git clone -b gh-pages --depth 1 git@github.com:srz-zumix/iutest.git gh-pages-temp
+  cd gh-pages-temp
+  git log -n -1
+  if `git log -n 1 | grep -q 'git@[0-9a-zA-Z]*'` ; then export PREVCOMMIT=`git log -n 1 | grep 'git@[0-9a-zA-Z]*' | head -1 | sed -e 's/.*git@\([0-9a-zA-Z]*\).*/\1/g'`; fi
+  cd ../
+  echo ${PREVCOMMIT}
+  export GH_PAGES_UPDATE=0
+  if ! git diff "$CI_COMMIT_ID:include" "$PREVCOMMIT:include" --exit-code --quiet; then export GH_PAGES_UPDATE=1; fi
+  if ! git diff "$CI_COMMIT_ID:docs" "$PREVCOMMIT:docs" --exit-code --quiet; then export GH_PAGES_UPDATE=1; fi
+else
+  export GH_PAGES_UPDATE=1
+  if [ ! "${DIRNAME}" =~ v.* ]; then
       DIRNAME=test
   fi
 fi
 
-git clone -b gh-pages --depth 1 git@github.com:srz-zumix/iutest.git gh-pages-temp
-cd gh-pages-temp
-git log -n -1
-if `git log -n 1 | grep -q 'git@[0-9a-zA-Z]*'` ; then export PREVCOMMIT=`git log -n 1 | grep 'git@[0-9a-zA-Z]*' | head -1 | sed -e 's/.*git@\([0-9a-zA-Z]*\).*/\1/g'`; fi
-cd ../
-echo $PREVCOMMIT
-export GH_PAGES_UPDATE=0
-if ! git diff $CI_COMMIT_ID:include $PREVCOMMIT:include --exit-code --quiet; then export GH_PAGES_UPDATE=1; fi
-if ! git diff $CI_COMMIT_ID:docs $PREVCOMMIT:docs --exit-code --quiet; then export GH_PAGES_UPDATE=1; fi
-
 doxygen --version
 dot --version || true
+
 cd docs
 export PATH=$PATH:$PWD
 echo $PATH
 git clone -b gh-pages git@github.com:srz-zumix/iutest.git gh-pages
 
 OUTDIR=gh-pages/${DIRNAME}
-
 if [ -d "${OUTDIR}" ]; then
   cd ${OUTDIR}
   git rm -r *
@@ -49,5 +50,8 @@ git add --all
 #export GIT_COMMIT_MESSAGE='update gh-pages git@$CI_COMMIT_ID'
 #git commit -m $GIT_COMMIT_MESSAGE
 git commit -m "update gh-pages git@$CI_COMMIT_ID"
-echo $GH_PAGES_UPDATE
-if [ $GH_PAGES_UPDATE == 1 ]; then git push; fi
+
+echo "$GH_PAGES_UPDATE"
+if [ $GH_PAGES_UPDATE == 1 ]; then
+  git push
+fi
