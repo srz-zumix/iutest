fac7e54827cbc1ee5c7cef33e75419714bf21511
srz_zumix <zumix.cpp@gmail.com>
2020-05-01 13:58:53 +0900

    fix lint

--- .ci/sync-svn-tag.sh
+++ .ci/sync-svn-tag.sh
@@ -3,8 +3,9 @@
 svn --version
 export SVN_URL=svn+ssh://srz_zumix@svn.osdn.jp/svnroot/iutest
 svn log "$SVN_URL/trunk" -l 1
-if $(svn log $SVN_URL/trunk -l 1 | grep -q 'git@[0-9a-zA-Z]*') ; then
-  export PREVCOMMIT=$(svn log $SVN_URL/trunk -l 1 | grep 'git@[0-9a-zA-Z]*' | head -1 | sed -e 's/.*git@\([0-9a-zA-Z]*\).*/\1/g')
+if svn log $SVN_URL/trunk -l 1 | grep -q 'git@[0-9a-zA-Z]*' ; then
+  PREVCOMMIT=$(svn log $SVN_URL/trunk -l 1 | grep 'git@[0-9a-zA-Z]*' | head -1 | sed -e 's/.*git@\([0-9a-zA-Z]*\).*/\1/g')
+  export PREVCOMMIT
 fi
 echo "$PREVCOMMIT"
 if [ "$CI_COMMIT_ID" = "$PREVCOMMIT" ]; then
