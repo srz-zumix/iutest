82a2f08442a74fd75e84104b41ee3b177d93bf7a
srz_zumix <zumix.cpp@gmail.com>
2020-05-01 21:01:15 +0900

    support local run

--- .github/actions/update-ghpages/entrypoint.sh
+++ .github/actions/update-ghpages/entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -exu
 
+
+if [ -z "${GITHUB_SHA}" ]; then
+  export GITHUB_SHA=$(git rev-parse HEAD)
+fi
 export CI_COMMIT_ID="${GITHUB_SHA}"
 
 export DIRNAME_="${GITHUB_REF#refs/heads/}"
@@ -19,7 +23,11 @@ dot -V || true
 cd docs
 export PATH=$PATH:"$PWD"
 git config --global http.sslverify false
-git clone --depth 1 -b gh-pages https://github.com/srz-zumix/iutest.git gh-pages
+if [ -z "${INPUT_GITHUB_TOKEN}" ]; then
+  git clone --depth 1 -b gh-pages git@github.com:srz-zumix/iutest.git gh-pages
+else
+  git clone --depth 1 -b gh-pages https://github.com/srz-zumix/iutest.git gh-pages
+fi
 
 OUTDIR="gh-pages/${DIRNAME}"
 if [ -d "${OUTDIR}" ]; then
@@ -33,15 +41,22 @@ make gh-pages GHPAGES_DIR="${OUTDIR}"
 make gh-pages-ci
 
 cd gh-pages
-if [ -z "${INPUT_GITHUB_EMAIL}" ]; then
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-else
-git config user.email "${INPUT_GITHUB_EMAIL}"
+
+if [ -n "${INPUT_GITHUB_TOKEN}" ]; then
+  if [ -z "${INPUT_GITHUB_EMAIL}" ]; then
+  git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+  else
+  git config user.email "${INPUT_GITHUB_EMAIL}"
+  fi
+  git config user.name "${GITHUB_ACTOR}"
 fi
-git config user.name "${GITHUB_ACTOR}"
 git add --all
 #export GIT_COMMIT_MESSAGE='update gh-pages git@$CI_COMMIT_ID'
 #git commit -m $GIT_COMMIT_MESSAGE
 git commit -m "update gh-pages git@$CI_COMMIT_ID"
 
-git push "https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/srz-zumix/iutest.git"
+if [ -z "${INPUT_GITHUB_TOKEN}" ]; then
+  git push
+else
+  git push "https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/srz-zumix/iutest.git"
+fi
