a86857061463a09a8e8aa463d57252747678e6e9
srz_zumix <zumix.cpp@gmail.com>
2020-05-02 21:42:40 +0900

    tag to latest

--- .github/actions/update-ghpages/entrypoint.sh
+++ .github/actions/update-ghpages/entrypoint.sh
@@ -16,7 +16,9 @@ export DIRNAME_="${GITHUB_REF#refs/heads/}"
 export DIRNAME="${DIRNAME_#refs/tags/}"
 
 if [ "${DIRNAME}" != 'master' ]; then
-  if [[ ! "${DIRNAME}" =~ v.* ]]; then
+  if [[ "${DIRNAME}" =~ v.* ]]; then
+      DIRNAME=latest
+  else
       DIRNAME=test
   fi
 fi
