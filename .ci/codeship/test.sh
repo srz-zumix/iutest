905448fb2cbfd62da214bbccc166914a32884014
srz_zumix <zumix.cpp@gmail.com>
2020-05-06 10:22:03 +0900

    test

--- .ci/codeship/test.sh
+++ .ci/codeship/test.sh
@@ -5,11 +5,11 @@ doxygen --version
 python --version
 export GH_PAGES_BRANCH=0
 if echo "$CI_BRANCH" | grep -q "gh-pages"; then export GH_PAGES_BRANCH=1; fi
-if [ $GH_PAGES_BRANCH == 0 ]; then make -C test/cpplint; fi
-if [ $GH_PAGES_BRANCH == 0 ]; then make -C test check_incg; fi
-if [ $GH_PAGES_BRANCH == 0 ]; then ! find projects -type f -print | xargs grep '[d-zD-Z]:\\'; fi
-if [ $GH_PAGES_BRANCH == 0 ]; then make -C test/docs version-test; fi
-if [ $GH_PAGES_BRANCH == 0 ]; then make -C test/docs doxygen-test; fi
-if [ $GH_PAGES_BRANCH == 0 ]; then sh .ci/check-crlf.sh; fi
-if [ $GH_PAGES_BRANCH == 0 ]; then make editorconfig-self-lint; fi
+if [ "$GH_PAGES_BRANCH" = 0 ]; then make -C test/cpplint; fi
+if [ "$GH_PAGES_BRANCH" = 0 ]; then make -C test check_incg; fi
+if [ "$GH_PAGES_BRANCH" = 0 ]; then ! find projects -type f -print | xargs grep '[d-zD-Z]:\\'; fi
+if [ "$GH_PAGES_BRANCH" = 0 ]; then make -C test/docs version-test; fi
+if [ "$GH_PAGES_BRANCH" = 0 ]; then make -C test/docs doxygen-test; fi
+if [ "$GH_PAGES_BRANCH" = 0 ]; then sh .ci/check-crlf.sh; fi
+if [ "$GH_PAGES_BRANCH" = 0 ]; then make editorconfig-self-lint; fi
 if git grep --cached -I -l -P '\r'; then exit 1; fi
