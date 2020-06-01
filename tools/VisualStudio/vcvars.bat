f695b6f83c1910fb1fda044bba0787c976f93d6f
srz_zumix <zumix.cpp@gmail.com>
2020-06-01 20:26:33 +0900

    fix vcperf/vcvars.bat

--- tools/VisualStudio/vcvars.bat
+++ tools/VisualStudio/vcvars.bat
@@ -9,6 +9,8 @@ if "%~1"=="" (
     exit 1
 )
 SET VSVER=%1
+SET ARGS=
+
 :check
 shift
 if "%~1"=="" goto final
@@ -16,7 +18,8 @@ set ARGS=%ARGS% %1
 goto check
 :final
 
-SET PATH=%PATH%;%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\
+which vswhere > nul 2>&1
+if errorlevel 1 SET PATH=%PATH%;%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\
 vswhere -version %VSVER% -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -find **/vcvarsall.bat > tmp.txt
 SET /p VCVARS=<tmp.txt
 del /F tmp.txt
