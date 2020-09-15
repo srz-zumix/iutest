cbf5e0c3c394d995a68d3a88a6cbacab5a26ed29
srz_zumix <zumix.cpp@gmail.com>
2020-09-15 22:08:46 +0900

    add -Wundef

--- utils/warning.mk
+++ utils/warning.mk
@@ -1,1 +1,1 @@
-IUTEST_CXX_STRICT_FLAGS+=-Wformat=2 -Wcast-qual -Wcast-align -Wwrite-strings -Wpointer-arith
+IUTEST_CXX_STRICT_FLAGS+=-Wformat=2 -Wcast-qual -Wcast-align -Wwrite-strings -Wpointer-arith -Wundef
