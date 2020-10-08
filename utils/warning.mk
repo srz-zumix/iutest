1e86a54e90055962d0d6bec2ce0a07fcbda5ecda
srz_zumix <zumix.cpp@gmail.com>
2020-10-08 11:52:14 +0900

    fix -Wimplicit-float-conversion

--- utils/warning.mk
+++ utils/warning.mk
@@ -1,4 +1,4 @@
 IUTEST_CXX_STRICT_FLAGS+= \
 	-Wformat=2 -Wcast-qual -Wcast-align -Wdouble-promotion \
 	-Wwrite-strings -Wpointer-arith -Wundef -Wunused-macros \
-	-Wmissing-noreturn
+	-Wmissing-noreturn -Wfloat-conversion
