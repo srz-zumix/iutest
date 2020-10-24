89ff12ba9438823eabe0bf41fbe65585a3043872
srz_zumix <zumix.cpp@gmail.com>
2020-10-24 11:19:54 +0900

    cl dME test

---
+++ tools/VisualStudio/cl-dME.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#
+# cl-dME.py
+#
+# usage:
+#   cl /B1cl-dME.py main.cpp
+#   cl /BXcl-dME.py main.cpp
+
+import os
+
+def main():
+    print(os.environ['MSC_CMD_FLAGS'])
+    print(os.environ['MSC_IDE_FLAGS'])
+
+if __name__ == "__main__":
+    main()
