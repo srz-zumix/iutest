8f8d9ab65e3491fe9013abc6c61e2c88f5a5cc8a
srz_zumix <zumix.cpp@gmail.com>
2021-05-08 14:40:05 +0900

    fix

--- test/type_param_register_exception_tests.cpp
+++ test/type_param_register_exception_tests.cpp
@@ -27,6 +27,9 @@
 #  define REGISTER_EXCEPTION_TEST   0
 #endif
 
+#if !defined(REGISTER_EXCEPTION_TEST_THROW_INT)
+#  define REGISTER_EXCEPTION_TEST_THROW_INT 0
+#endif
 
 #if IUTEST_HAS_STREAM_BUFFER
     ::iutest::detail::IUStreamBuffer<> stderr_capture(stderr);
@@ -38,26 +41,22 @@ struct exception_value
 {
 };
 
-struct exception_int_value
-{
-};
-
 namespace iutest
 {
 namespace detail
 {
+
 template<>
 ::std::string MakeIndexTypedTestName<exception_value>(const char* basename, size_t index)
 {
-    IUTEST_SUPPRESS_UNREACHABLE_CODE_WARNING(throw ::std::runtime_error("HOGE"));
-    return MakeIndexTestName(basename, index);
-}
-template<>
-::std::string MakeIndexTypedTestName<exception_int_value>(const char* basename, size_t index)
-{
+#if REGISTER_EXCEPTION_TEST_THROW_INT
     IUTEST_SUPPRESS_UNREACHABLE_CODE_WARNING(throw 42);
+#else
+    IUTEST_SUPPRESS_UNREACHABLE_CODE_WARNING(throw ::std::runtime_error("HOGE"));
+#endif
     return MakeIndexTestName(basename, index);
 }
+
 } // end of namespace detail
 } // end of namespace iutest
 
@@ -73,7 +72,6 @@ IUTEST_TYPED_TEST_P(RegisterExceptionTypeParamTest, Noop)
 IUTEST_REGISTER_TYPED_TEST_SUITE_P(RegisterExceptionTypeParamTest, Noop);
 
 IUTEST_INSTANTIATE_TYPED_TEST_SUITE_P(A, RegisterExceptionTypeParamTest, ::iutest::Types<exception_value>);
-IUTEST_INSTANTIATE_TYPED_TEST_SUITE_P(B, RegisterExceptionTypeParamTest, ::iutest::Types<exception_int_value>);
 
 #endif
 
@@ -87,17 +85,15 @@ int main(int argc, char* argv[])
 #if REGISTER_EXCEPTION_TEST
     ::iutest::IUTEST_FLAG(warning_into_error) = false;
 #if IUTEST_HAS_STREAM_BUFFER
-    // IUTEST_EXPECT_STRIN(
-    //     "IUTEST_INSTANTIATE_TYPED_TEST_SUITE_P register tests failed... (what: HOGE)\n"
-    //     , stderr_capture.GetStreamString() );
-    // IUTEST_EXPECT_STRIN(
-    //     "IUTEST_INSTANTIATE_TYPED_TEST_SUITE_P register tests failed...\n"
-    //     , stderr_capture.GetStreamString() );
-
-    // It will be fflushed and will be overwritten
+#if REGISTER_EXCEPTION_TEST_THROW_INT
     IUTEST_EXPECT_STRIN(
-        "IUTEST_INSTANTIATE_TYPED_TEST_SUITE_P register tests failed...\n\nwhat: HOGE)\n"
+        "IUTEST_INSTANTIATE_TYPED_TEST_SUITE_P register tests failed...\n"
         , stderr_capture.GetStreamString() );
+#else
+    IUTEST_EXPECT_STRIN(
+        "IUTEST_INSTANTIATE_TYPED_TEST_SUITE_P register tests failed... (what: HOGE)\n"
+        , stderr_capture.GetStreamString() );
+#endif
 #endif
 #endif
     return IUTEST_RUN_ALL_TESTS();
