931a77a54a1f403973de8b3687881a697445ed6f
srz_zumix <zumix.cpp@gmail.com>
2021-12-17 20:45:34 +0900

    fix

--- include/internal/iutest_stdlib_defs.hpp
+++ include/internal/iutest_stdlib_defs.hpp
@@ -639,7 +639,7 @@
 #endif
 
 #if !defined(iu_va_end)
-#  if _MSC_VER
+#  if defined(_MSC_VER)
 #    define iu_va_end   IUTEST_ATTRIBUTE_GSL_SUPPRESS(es.47) va_end
 #  else
 #    define iu_va_end   va_end
