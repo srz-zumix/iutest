a5e5abe0b4a317a9a1647f8bcf5aed0b5ed872c2
srz_zumix <zumix.cpp@gmail.com>
2022-02-17 11:18:29 +0900

    fix C26440

--- include/internal/iutest_pragma.hpp
+++ include/internal/iutest_pragma.hpp
@@ -482,6 +482,7 @@
 #if   defined(_MSC_VER)
 #  define IUTEST_PRAGMA_COREGUIDELINE_DISABLE_BEGIN()   IUTEST_PRAGMA_MSC_WARN_PUSH() \
                                                         IUTEST_PRAGMA_MSC_WARN_DISABLE(26426) \
+                                                        IUTEST_PRAGMA_MSC_WARN_DISABLE(26440) \
                                                         IUTEST_PRAGMA_MSC_WARN_DISABLE(26477) \
                                                         IUTEST_PRAGMA_MSC_WARN_DISABLE(26497) \
                                                         IUTEST_PRAGMA_MSC_WARN_DISABLE(26814)
