f30c7c34bb79032902527f583e87bddba905e8ff
srz_zumix <zumix.cpp@gmail.com>
2022-03-03 21:32:02 +0900

    no_fwd_decls

--- .github/actions/iwyu/entrypoint.sh
+++ .github/actions/iwyu/entrypoint.sh
@@ -8,6 +8,7 @@ clang --version
 
 LOGICAL_CPU_NUM=`grep processor /proc/cpuinfo | wc -l`
 IWYU_TOOL_OPTION=${IWYU_TOOL_OPTION:--j${LOGICAL_CPU_NUM}}
+IWYU_ARGS=${IWYU_ARGS:-}
 
 # make working directory link
 ln -sf "`pwd`/" /work
@@ -15,4 +16,4 @@ ln -sf "`pwd`/" /work
 mkdir -p /tmp/build && cd /tmp/build
 
 CC="clang" CXX="clang++" cmake -DCMAKE_C_COMPILER="clang" -DCMAKE_CXX_COMPILER="clang++" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON $*
-iwyu_tool.py ${IWYU_TOOL_OPTION} -p .
+iwyu_tool.py ${IWYU_TOOL_OPTION} -p . -- ${IWYU_ARGS}
