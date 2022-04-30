f242aa403d091fe72f2d63fa8ac305eaf68861b8
srz_zumix <zumix.cpp@gmail.com>
2022-04-30 22:21:58 +0900

    up

--- test/stdlib_feature_tests/stdlib_feature_test.cpp
+++ test/stdlib_feature_tests/stdlib_feature_test.cpp
@@ -18,19 +18,4 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#define PP_TOSTRING(x)      PP_TOSTRING_(x)
-#define PP_TOSTRING_(x)     #x
-#ifdef FEATURE_TEST_EXECUTABLE
-#define SHOW_MACRO(macro)   printf("#define %s  %s\n", #macro, PP_TOSTRING(macro))
-#define SHOW_UNDEF(macro)   printf("#undef  %s\n", #macro)
-#else
-#define SHOW_MACRO(macro)   define #macro macro
-#define SHOW_UNDEF(macro)   undef  #macro
-#endif
-
-int main()
-{
-    #include "show_macros.i"    // NOLINT
-    return 0;
-}
-
+#include "show_macros.i"    // NOLINT
