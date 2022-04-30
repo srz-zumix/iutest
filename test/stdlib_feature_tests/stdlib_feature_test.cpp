7d37eeddfdac62469f211e4aa2f5b6344cc6f2b2
srz_zumix <zumix.cpp@gmail.com>
2022-04-30 15:28:45 +0900

    up

--- test/stdlib_feature_tests/stdlib_feature_test.cpp
+++ test/stdlib_feature_tests/stdlib_feature_test.cpp
@@ -20,9 +20,13 @@
 
 #define PP_TOSTRING(x)      PP_TOSTRING_(x)
 #define PP_TOSTRING_(x)     #x
-#define SHOW_MACRO(macro)   SHOW_MACRO_I(#macro, PP_TOSTRING(macro))
-#define SHOW_MACRO_I(name, value)   printf("#define %s  %s\n", name, value)
-#define SHOW_UNDEF(macro)           printf("#undef  %s\n", #macro)
+#ifdef FEATURE_TEST_EXECUTABLE
+#define SHOW_MACRO(macro)   printf("#define %s  %s\n", #macro, PP_TOSTRING(macro))
+#define SHOW_UNDEF(macro)   printf("#undef  %s\n", #macro)
+#else
+#define SHOW_MACRO(macro)   define #macro macro
+#define SHOW_UNDEF(macro)   undef  #macro
+#endif
 
 int main()
 {
