fab948436fef3a7de670bce64efac0b566d08c32
srz_zumix <zumix.cpp@gmail.com>
2022-06-02 21:02:42 +0900

    add test

--- test/port_tests.cpp
+++ test/port_tests.cpp
@@ -6,7 +6,7 @@
  *
  * @author      t.shirayanagi
  * @par         copyright
- * Copyright (C) 2016, Takazumi Shirayanagi\n
+ * Copyright (C) 2016-2022, Takazumi Shirayanagi\n
  * The new BSD License is applied to this software.
  * see LICENSE
 */
@@ -21,7 +21,7 @@
 IUTEST(PortableTest, SetGetEnv)
 {
     const char* env_name = "IUTEST_PORT_TEST_SETENV";
-    if( ::iutest::internal::posix::SetEnv(env_name, "test", 0) != 0 ) IUTEST_SKIP();
+    IUTEST_ASSUME_NE(0, ::iutest::internal::posix::SetEnv(env_name, "test", 0));
     IUTEST_ASSERT_STREQ("test", ::iutest::internal::posix::GetEnv(env_name));
 
     IUTEST_EXPECT_EQ(0, ::iutest::internal::posix::SetEnv(env_name, "hoge", 0));
@@ -34,11 +34,17 @@ IUTEST(PortableTest, SetGetEnv)
 IUTEST(PortableTest, PutGetEnv)
 {
     const char* env_name = "IUTEST_PORT_TEST_PUTENV";
-    if( ::iutest::internal::posix::PutEnv("IUTEST_PORT_TEST_PUTENV=test") != 0 ) IUTEST_SKIP();
+    IUTEST_ASSUME_NE(0, ::iutest::internal::posix::PutEnv("IUTEST_PORT_TEST_PUTENV=test"));
     IUTEST_ASSERT_STREQ("test", ::iutest::internal::posix::GetEnv(env_name));
 
     IUTEST_EXPECT_NE(-1, ::iutest::internal::posix::PutEnv("IUTEST_PORT_TEST_PUTENV=hoge"));
     IUTEST_ASSERT_STREQ("hoge", ::iutest::internal::posix::GetEnv(env_name));
+}
+
+IUTEST(PortableTest, FileStat)
+{
+    ::iutest::internal::posix::StatStruct st;
+    IUTEST_ASSERT_EQ(0, ::iutest::internal::posix::FileStat(0, &st));
 }
 
 #ifdef UNICODE
