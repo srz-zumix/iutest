3547d651f6ac5ee9c27ef0ebcb7d073684b5fccd
srz_zumix <zumix.cpp@gmail.com>
2022-07-03 12:06:35 +0900

    fix for mingw

--- include/internal/iutest_port.hpp
+++ include/internal/iutest_port.hpp
@@ -108,7 +108,7 @@ inline void Abort() { abort(); }
 
 inline off_t FileSeek(FILE* fp, off_t pos, int origin)
 {
-#if defined(_MSC_VER) || defined(IUTEST_OS_WINDOWS_MINGW)
+#if defined(_MSC_VER)
     return _fseeki64(fp, pos, origin);
 #elif (!defined(_FILE_OFFSET_BITS) || _FILE_OFFSET_BITS != 64) || (defined(defined) && _POSIX_C_SOURCE >= 200112L)
     return fseeko64(fp, pos, origin);
@@ -119,7 +119,7 @@ inline off_t FileSeek(FILE* fp, off_t pos, int origin)
 
 inline off_t FileTell(FILE* fp)
 {
-#if defined(_MSC_VER) || defined(IUTEST_OS_WINDOWS_MINGW)
+#if defined(_MSC_VER)
     return _ftelli64(fp);
 #elif (!defined(_FILE_OFFSET_BITS) || _FILE_OFFSET_BITS != 64) || (defined(defined) && _POSIX_C_SOURCE >= 200112L)
     return ftello64(fp);
